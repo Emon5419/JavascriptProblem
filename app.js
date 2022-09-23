e4d8e3d96251c370aecac94872c59559245f8cc1
Mohammad Alauddin Emon <95302046+Emon5419@users.noreply.github.com>
2022-09-23 11:52:38 +0600

    problem2Solve

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+
+//Problem:1  radianToDegree
+
 function radianToDegree(radian) {
    const pi = Math.PI;
    return radian * (180 / pi);
 }
-console.log(radianToDegree(15).toFixed(2))
+const result = radianToDegree(15).toFixed(2);
+
+// console.log(result);
+
+// Problem: 2  isJavaScriptFile
+
+function isJavaScriptFile(filename) {
+   const file = 'app.js';
+
+   if (filename == file) {
+      return true;
+   }
+   else {
+      return false;
+   }
+}
+// console.log(isJavaScriptFile('app.js'))
